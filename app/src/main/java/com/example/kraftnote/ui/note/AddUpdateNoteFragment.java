32657b229246a63bfb301dae33f85690522176f6
Roshan Kharel <rosn.khrl@gmail.com>
2020-10-11 08:40:40 +1100

    Note editor fragment - show confirmation dialog on back/cancel pressed

--- app/src/main/java/com/example/kraftnote/ui/note/AddUpdateNoteFragment.java
+++ app/src/main/java/com/example/kraftnote/ui/note/AddUpdateNoteFragment.java
@@ -1,10 +1,13 @@
 package com.example.kraftnote.ui.note;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,6 +19,7 @@ import com.example.kraftnote.R;
 import com.example.kraftnote.persistence.entities.Category;
 import com.example.kraftnote.persistence.viewmodels.CategoryViewModel;
 import com.example.kraftnote.ui.category.CategoryTabLayout;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
@@ -24,7 +28,18 @@ import java.util.List;
 public class AddUpdateNoteFragment extends Fragment {
     private NavController navController;
     private CategoryViewModel categoryViewModel;
+    private MaterialButton closeEditorButton;
+    private MaterialButton saveNoteButton;
     private List<Category> categories = new ArrayList<>();
+
+    // This callback will only be called when AddUpdateNoteFragment is at least started
+    private
+    OnBackPressedCallback onBackPressedCallback = new OnBackPressedCallback(true) {
+        @Override
+        public void handleOnBackPressed() {
+            AddUpdateNoteFragment.this.gotoNoteFragment();
+        }
+    };
 
     @Override
     public View onCreateView(
@@ -32,6 +47,11 @@ public class AddUpdateNoteFragment extends Fragment {
             @Nullable Bundle savedInstanceState
     ) {
         categoryViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
+
+
+        requireActivity().getOnBackPressedDispatcher()
+                .addCallback(getViewLifecycleOwner(), onBackPressedCallback);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_update_note, container, false);
     }
@@ -39,21 +59,21 @@ public class AddUpdateNoteFragment extends Fragment {
     public void onViewCreated(@NonNull final View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        initializeProperties();
-        findViews(view);
+        initializeProperties(view);
         listenEvents();
     }
 
-    private void initializeProperties() {
+    private void initializeProperties(@NonNull final View view) {
         navController = NavHostFragment.findNavController(this);
-    }
-
-    private void findViews(@NonNull final View view) {
-
+        closeEditorButton = view.findViewById(R.id.close_editor_button);
+        saveNoteButton = view.findViewById(R.id.save_note_button);
     }
 
     private void listenEvents() {
         categoryViewModel.getAll().observe(getViewLifecycleOwner(), this::categoriesMutated);
+        closeEditorButton.setOnClickListener(v -> {
+            gotoNoteFragment();
+        });
     }
 
     private void categoriesMutated(List<Category> categories) {
@@ -61,7 +81,16 @@ public class AddUpdateNoteFragment extends Fragment {
     }
 
     private void gotoNoteFragment() {
-        // TODO confirm before going back
-        navController.navigate(R.id.action_AddUpdateNoteFragment_to_NoteFragment);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+
+        builder
+                .setTitle(R.string.confirmation_required)
+                .setMessage(R.string.do_you_want_to_discard_the_changes)
+                .setCancelable(false)
+                .setNegativeButton(R.string.no, null)
+                .setPositiveButton(R.string.yes, (dialog, which) -> {
+                    navController.navigate(R.id.action_AddUpdateNoteFragment_to_NoteFragment);
+                })
+                .show();
     }
 }
