c654e3b51ce249b7eaf7bad813f73b4ea3df5099
Roshan Kharel <rosn.khrl@gmail.com>
2020-10-21 23:46:20 +1100

    splash screen

---
+++ app/src/main/java/com/example/kraftnote/ui/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.kraftnote.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kraftnote.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = SplashActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, 500);
+    }
+}
